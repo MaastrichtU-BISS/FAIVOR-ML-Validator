cda55b574b4f25ab07aa20d8c5004525349d921f
Saba Amiri <s.amiri@esciencecenter.nl>
2025-01-22 09:22:21 +0100

    fixed paths

--- src/faivor/metrics/regression/__init__.py
+++ src/faivor/metrics/regression/__init__.py
@@ -1,6 +1,1 @@
-"""
-This package contains modules for regression metrics, categorized into performance,
-fairness, and explainability.
-"""
 __all__ = ["performance", "fairness", "explainability"]
-
