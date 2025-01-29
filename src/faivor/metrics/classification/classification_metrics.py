e46373647d27023e6f9d5f0d9e35f8fb8ae0f126
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2025-01-29 15:55:17 +0100

    Add __init__ and fix import paths

--- src/faivor/metrics/classification/classification_metrics.py
+++ src/faivor/metrics/classification/classification_metrics.py
@@ -1,3 +1,3 @@
-from ..config_loader import load_metrics
+from faivor.metrics.config_loader import load_metrics
 
 PERFORMANCE_METRICS, FAIRNESS_METRICS, EXPLAINABILITY_METRICS = load_metrics("classification/classification_metrics.yaml")
