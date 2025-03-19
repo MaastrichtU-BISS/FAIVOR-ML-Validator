3172c2b504e7ba91b37ab44b5001a6fb2c96f813
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2025-03-19 10:00:26 +0100

    Metrics test placeholder

--- tests/test_model_execution.py
+++ tests/test_model_execution.py
@@ -19,3 +19,19 @@ def test_model_execution(shared_datadir):
 
     assert prediction is not None, "Model execution should return a prediction."
     assert isinstance(prediction, list), "Prediction result should be a dictionary."
+
+
+    def test_model_metrics(shared_datadir):
+        model_dir = shared_datadir / "models"
+        metadata_json = json.loads((model_dir / "pilot-model_1" / "metadata.json").read_text(encoding="utf-8"))
+        model_metadata = ModelMetadata(metadata_json)
+        assert model_metadata.docker_image, "Docker image name should be provided"
+
+        csv_path = model_dir / "pilot-model_1" / "data.csv"
+        inputs, _ = create_json_payloads(model_metadata, csv_path)
+        try:
+            prediction = execute_model(model_metadata, inputs)
+        except Exception as e:
+            raise RuntimeError(f"Model execution failed: {e}")
+
+        #TODO: compute_metric(prediction, expected_output)
