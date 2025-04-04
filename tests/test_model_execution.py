7a3b505c2f7c164b2b34ab0e208b8ff4b9ab72ac
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2025-04-04 15:18:22 +0200

    Update model execution params

--- tests/test_model_execution.py
+++ tests/test_model_execution.py
@@ -3,13 +3,15 @@ from faivor.model_metadata import ModelMetadata
 from faivor.parse_data import create_json_payloads
 from faivor.run_docker import execute_model
 
+model:str = "pilot-model_2"
+
 def test_model_execution(shared_datadir):
     model_dir = shared_datadir / "models"
-    metadata_json = json.loads((model_dir / "pilot-model_1" / "metadata.json").read_text(encoding="utf-8"))
+    metadata_json = json.loads((model_dir / model / "metadata.json").read_text(encoding="utf-8"))
     model_metadata = ModelMetadata(metadata_json)
     assert model_metadata.docker_image, "Docker image name should be provided"
 
-    csv_path = model_dir / "pilot-model_1" / "data.csv"
+    csv_path = model_dir / model / "data.csv"
     inputs, _ = create_json_payloads(model_metadata, csv_path)
     try:
         prediction = execute_model(model_metadata, inputs)
