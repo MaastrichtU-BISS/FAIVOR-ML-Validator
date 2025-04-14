259f26ab99bdb9fefd57486e8a9f3541565f883d
kateakhmad <99681200+kateakhmad@users.noreply.github.com>
2025-04-14 14:57:09 +0200

    added new classification metrics

--- src/faivor/metrics/classification/performance.py
+++ src/faivor/metrics/classification/performance.py
@@ -6,3 +6,56 @@ def error_rate(y_true, y_pred):
     y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
     return 1 - skm.accuracy_score(y_true, y_pred)
 
+def negative_predictive_value(y_true, y_pred) -> float:
+    """
+    Calculates negative predictive value.
+
+    Parameters
+    ----------
+    y_true : array-like of shape (n_samples,)
+        The true target values.
+    y_pred : array-like of shape (n_samples,)
+
+    Returns
+    -------
+    float
+        The negative predictive value.
+    """
+    y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
+    tn, fp, fn, tp=skm.confusion_matrix(y_true,y_pred).ravel()
+    return tn/(tn+fn)
+
+def positive_predictive_value(y_true, y_pred) -> float:
+    """
+    Calculates positive predictive value for classification.
+    Parameters
+    ----------
+    y_true : array-like of shape (n_samples,)
+        The true target values.
+    y_pred : array-like of shape (n_samples,)
+
+    Returns
+    -------
+    float
+        The positive predictive value.
+        """
+    y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
+    tn, fp, fn, tp=skm.confusion_matrix(y_true,y_pred).ravel()
+    return tp/(tp+fp)
+
+def specificity(y_true, y_pred) -> float:
+    """
+    Calculates specificity for classification.
+    Parameters
+    ----------
+    y_true : array-like of shape (n_samples,)
+        The true target values.
+    y_pred : array-like of shape (n_samples,)
+
+    Returns
+    -------
+    float
+        The specificity.
+    """
+    y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
+    return skm.recall_score(y_true,y_pred,pos_label=0)
